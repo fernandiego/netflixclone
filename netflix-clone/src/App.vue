f4a67902684244257cd6630379eb230711c63f47
fox <fernandodiego.sn@gmail.com>
2023-11-17 14:20:10 -0300

    First video added

--- netflix-clone/src/App.vue
+++ netflix-clone/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {onMounted, ref} from "vue";
+import movies from "./movies.json"
 
 import Magnfy from 'vue-material-design-icons/Magnify.vue';
 import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
@@ -15,12 +16,16 @@ import {storeToRefs} from 'pinia';
 const useMovie = useMovieStore()
 const {movie, showFullVideo} = storeToRefs(useMovie)
 
+onMounted(() => {
+  setTimeout(() => movie.value = movies[0][0], 100)
+})
+
 </script>
 
 <template>
   <div class="fixed w-full h-screen bg-black">
     <div v-if="!showFullVideo" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
-      N
+      <img class="absolute top-0 w-[35px] mt-10 ml-10" src="/images/netflix-logo.png">
       <div>
         <div class="py-2 mx-10 my-6">
           <Magnfy fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
@@ -31,15 +36,26 @@ const {movie, showFullVideo} = storeToRefs(useMovie)
         <div class="py-2 mx-10 my-6">
           <TrendingUp fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
         </div>
-         <div class="py-2 mx-10 my-6">
+        <div class="py-2 mx-10 my-6">
           <Television fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
         </div>
-         <div class="py-2 mx-10 my-6">
+        <div class="py-2 mx-10 my-6">
           <MovieOutline fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
         </div>
-         <div class="py-2 mx-10 my-6">
+        <div class="py-2 mx-10 my-6">
           <Plus fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
         </div>
+      </div>
+    </div>
+    <div v-if="!showFullVideo">
+      <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
+        <video
+            v-if="movie"
+            :src="'/videos/'+movie.name+'.mp4'"
+            autoplay
+            loop
+            class="absolute z-0 h-[600] right-0 top-0"
+        />
       </div>
     </div>
   </div>
