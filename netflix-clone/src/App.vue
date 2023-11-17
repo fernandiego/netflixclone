2c383aa62908aaf926e689bd9f421b22d1e87b29
fox <fernandodiego.sn@gmail.com>
2023-11-17 13:47:43 -0300

    Frameworks for visuals and images and short videos to show content

--- netflix-clone/src/App.vue
+++ netflix-clone/src/App.vue
@@ -1,9 +1,46 @@
 <script setup>
+import {onMounted, ref} from "vue";
+
+import Magnfy from 'vue-material-design-icons/Magnify.vue';
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
+import TrendingUp from 'vue-material-design-icons/TrendingUp.vue';
+import Television from 'vue-material-design-icons/Television.vue';
+import MovieOutline from 'vue-material-design-icons/MovieOutline.vue';
+import Plus from 'vue-material-design-icons/Plus.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+
+import {useMovieStore} from './stores/movie';
+import {storeToRefs} from 'pinia';
+
+const useMovie = useMovieStore()
+const {movie, showFullVideo} = storeToRefs(useMovie)
+
 </script>
 
 <template>
-
-  <h1 class="text-red-400">
-      Hello world!
-    </h1>
+  <div class="fixed w-full h-screen bg-black">
+    <div v-if="!showFullVideo" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
+      N
+      <div>
+        <div class="py-2 mx-10 my-6">
+          <Magnfy fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
+          <HomeOutline fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <TrendingUp fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+         <div class="py-2 mx-10 my-6">
+          <Television fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+         <div class="py-2 mx-10 my-6">
+          <MovieOutline fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+         <div class="py-2 mx-10 my-6">
+          <Plus fill-color="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
