5ca6acdc72b56c106dcad5d6f2123aea3a8463f0
fox <fernandodiego.sn@gmail.com>
2023-11-17 18:40:57 -0300

    Namne of the movie, genre, description, categories

--- netflix-clone/src/App.vue
+++ netflix-clone/src/App.vue
@@ -10,6 +10,9 @@ import MovieOutline from 'vue-material-design-icons/MovieOutline.vue';
 import Plus from 'vue-material-design-icons/Plus.vue';
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
 
+import MovieDetails from '@/components/MovieDetails.vue';
+import VideoCarousel from '@/components/VideoCarousel.vue';
+
 import {useMovieStore} from './stores/movie';
 import {storeToRefs} from 'pinia';
 
@@ -49,13 +52,21 @@ onMounted(() => {
     </div>
     <div v-if="!showFullVideo">
       <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
+
+        <MovieDetails v-if="movie" :movie="movie"/>
+
         <video
             v-if="movie"
             :src="'/videos/'+movie.name+'.mp4'"
             autoplay
             loop
-            class="absolute z-0 h-[600] right-0 top-0"
+            class="absolute z-0 h-[600px] right-0 top-0"
         />
+      </div>
+      <div class="fixed z-30 bottom-0 right-0 w-full h-[55%] pl-[120px] overflow-y-auto">
+        <VideoCarousel class="pb-14 pt-14" category="Popular Movies" :movies="movies[0]" />
+        <VideoCarousel class="pb-14" category="Horror Movies" :movies="movies[1]" />
+        <VideoCarousel class="pb-32" category="Featured Movies" :movies="movies[2]" />
       </div>
     </div>
   </div>
